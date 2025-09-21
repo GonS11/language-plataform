69bbe1b34ee2513a1502c98a6d156ba8c1989a1b
“Gonzalo” <gonzalos.camara@gmail.com>
2025-09-21 11:47:04 +0200

    Model + relations

--- language-plataform/app/Models/Lesson.php
+++ language-plataform/app/Models/Lesson.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Lesson extends Model
 {
-    //
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function level()
+    {
+        return $this->belongsTo(Level::class);
+    }
 }
