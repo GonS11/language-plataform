2e97722da62f72958241de50f2399bcab536b22b
“Gonzalo” <gonzalos.camara@gmail.com>
2025-09-21 12:30:45 +0200

    User + Roles + Permission seeder

--- language-plataform/app/Models/User.php
+++ language-plataform/app/Models/User.php
@@ -6,19 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens;
-
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory;
-    use HasProfilePhoto;
-    use Notifiable;
-    use TwoFactorAuthenticatable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
--- language-plataform/database/seeders/DatabaseSeeder.php
+++ language-plataform/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(LevelSeeder::class);
+        $this->call([LevelSeeder::class, RoleSeeder::class, UserSeeder::class]);
     }
 }
